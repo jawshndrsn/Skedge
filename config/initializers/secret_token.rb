c86b1c6d15172c408ba1152bb8b0a9f136b783e3
Josh Anderson <josh.anderson@sendgrid.com>
2013-03-18 15:01:14 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skedge::Application.config.secret_token = '674ebb9a64aeac100e2650703029ebb95b9a5c2a81dd35102d77cc6e4a3dbdf6cc2ace6051a7bade5c42f4ea44fbc67f85fdbb6e731486da638d1a5b7b60c381'
